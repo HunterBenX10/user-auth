b82c7f9da297b7327eda114297919cfaf482bff9
Ben <emmanuelbeneboh@gmail.com>
2025-10-13 22:28:19 -0400

    Inital commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
+// const uri = "mongodb+srv://eeb78:<db_password>@cluster1.uow2sfc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
